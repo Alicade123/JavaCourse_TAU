1ad510e9e259d81a81588aed47c5109b736676db
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-30 23:22:09 +0200

    feat(chapter7): add array learning examples

---
+++ src/chapter7/ArrayLearning.java
@@ -0,0 +1,29 @@
+package chapter7;
+
+import java.util.Random;
+
+public class ArrayLearning {
+    static private final int RANGE = 6;
+
+    public static void main(String[] args){
+        int[] myArray = getRandomNumbers();
+        printArray(myArray);
+    }
+    public static int[] getRandomNumbers(){
+        int[] numbers = new  int[RANGE];
+        Random random = new Random();
+        for(int i=0; i<RANGE;i++){
+            numbers[i]=random.nextInt(1,69);
+        }
+        return numbers;
+    }
+    public static void printArray(int [] arr){
+        for(int i=0; i<RANGE; i++){
+            System.out.print(arr[i]+" | ");
+        }
+    }
+
+}
+
+
+

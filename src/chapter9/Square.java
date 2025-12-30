a77f00b3a7fd5c771db918ce59c1bbfcd704987a
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-30 23:21:37 +0200

    feat(chapter9): add Square class implementation

--- src/chapter9/Square.java
+++ src/chapter9/Square.java
@@ -1,4 +1,14 @@
 package chapter9;
 
-public class Square {
+public class Square extends Rectangle{
+    //Todo : Method overriding
+    @Override
+    public double calculatePerimeter(){
+        return (sides*sides);
+    }
+
+    //Todo : Method overloading
+    public void myName(String name){
+        System.out.println("Hei there I'm "+name);
+    }
 }
